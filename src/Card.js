7dd2ea111a7eec432c9bfda4bc40210cd902228e
W1dee <nickwide@bk.ru>
2020-11-15 22:23:40 +0300

    Second task

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import './Card.css'
+import React, {Component} from 'react';
+import './Card.css';
 
-function Card(props) {
-    return (
-        <div>
-            <div className="Div-card">
-                <div className="Div-card-caption">
-                    <h1>КАРТОЧКА</h1>
-                </div>
-                <hr />
-                <div className="Div-card-text">
-                    <p>Здесь должен быть какой-то текст, но вместо него здесь смайлик ^w^</p>
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            cardStyleName: "Div-card",
+            captionStyleName: "Caption",
+            checked: false
+        };
+        this.handleChange=this.handleChange.bind(this);
+    }
+    handleChange() {
+        if(!this.state.checked) {
+            this.setState({
+                cardStyleName: "Div-card-new",
+                captionStyleName: "Caption-new",
+                checked: true
+            })
+        }
+        else {
+            this.setState({
+                cardStyleName: "Div-card",
+                captionStyleName: "Caption",
+                checked: false
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={this.state.cardStyleName}>
+                    <div className="Div-card-caption">
+                        <h1 className={this.state.captionStyleName}>КАРТОЧКА</h1>
+                        <label>
+                            <input type="checkbox" onChange={this.handleChange} />
+                            <div className="Checkmark" />
+                        </label>
+                    </div>
+                    <hr />
+                    <div className="Div-card-text">
+                        <p>Здесь должен быть какой-то текст, но вместо него здесь смайлик ^w^</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Card;
